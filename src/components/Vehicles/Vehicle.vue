a068f4b864566dd9c15e3be37431ac5b73e0abd3
Fabio Z Tessitore <fabioztessitore@gmail.com>
2021-04-11 19:13:50 +0200

    .

--- src/components/Vehicles/Vehicle.vue
+++ src/components/Vehicles/Vehicle.vue
@@ -1,7 +1,9 @@
 <template>
 	<div class="vehicle" @click="$emit('clicked', data.id)">
-		<span class="vehicle__data">{{ data.brand }} - {{ data.model }} - {{ data.license_plate }}</span>
-		<span class="vehicle__actions" @click.stop="$emit('action1', data.id)">&times;</span>
+		<span class="vehicle__data">{{ data.brand }} {{ data.model }} - {{ data.license_plate }}</span>
+		<span class="vehicle__actions">
+			<button class="btn btn--flat" @click.stop="$emit('action1', data.id)">&times;</button>
+		</span>
 	</div>
 </template>
 
